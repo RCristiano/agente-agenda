1084b6061b7cc150a6a3db2722fc54d0e4d864bb
Rodrigo Cristiano <rcristianofv@hotmail.com>
2021-11-22 07:05:10 -0300

    Improved register form

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -4,28 +4,56 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
-from wtforms.validators import Email, DataRequired
+from wtforms import TextField, PasswordField, BooleanField
+from wtforms.validators import (
+    Email,
+    DataRequired,
+    EqualTo,
+    Length,
+    ValidationError,
+)
 
 # login and registration
 
 
 class LoginForm(FlaskForm):
-    username = TextField('Username',
-                         id='username_login',
-                         validators=[DataRequired()])
-    password = PasswordField('Password',
-                             id='pwd_login',
-                             validators=[DataRequired()])
+    username = TextField(
+        "Username", id="username_login", validators=[DataRequired()]
+    )
+    password = PasswordField(
+        "Password", id="pwd_login", validators=[DataRequired()]
+    )
 
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username',
-                         id='username_create',
-                         validators=[DataRequired()])
-    email = TextField('Email',
-                      id='email_create',
-                      validators=[DataRequired(), Email()])
-    password = PasswordField('Password',
-                             id='pwd_create',
-                             validators=[DataRequired()])
+    username = TextField(
+        "Username",
+        id="username_create",
+        validators=[DataRequired(), Length(min=5, max=64)],
+    )
+    email = TextField(
+        "Email", id="email_create", validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        "Password", id="pwd_create", validators=[DataRequired(), Length(min=6)]
+    )
+    confirm_password = PasswordField(
+        "Repeat Password",
+        id="confirm_pwd_create",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="Passwords must match"),
+        ],
+    )
+    prestador = BooleanField(
+        "Sou profissional, prestador ou possuo um estabelecimento.",
+        id="prestador_create",
+    )
+
+    def validate_username(self, username):
+        excluded_chars = " *?!'^+%&/()=}][{$#"
+        for char in username.data:
+            if char in excluded_chars:
+                raise ValidationError(
+                    f"Character {char} is not allowed in username."
+                )
